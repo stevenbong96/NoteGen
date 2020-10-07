17258cd349072e1abe789dc6ca26c7f95e7ddf23
Steven Bong <stevenseal99@yahoo.com>
2020-10-07 14:40:13 -0700

    Add start script file in package.json. Had issue with dsiplaying HTML files in my server file

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,23 @@
 // Declare the npm package
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 // Declare the express variables
 var app = express();
 var PORT = 8080;
+// var PORT = process.env.PORT || 8080;
 
 // Set the express for parsing the data
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+// app.use(express.static("public"));
 
 // Declare the variables
-// var notesArr = [];
+var notesArr = [{
+    "title": "Bla bla bla",
+    "text": "wassup",
+}];
 
 // Declare the routes to display HTML page
 // Display the index.html file
@@ -20,14 +26,18 @@ app.get("*", function(request, response){
 })
 
 // Display the notes.html
-app.get("/notes", function(request, response){
-    response.sendFile(path.join(__dirname, "notes.html"));
-})
+// app.get("/notes", function(request, response){
+//     response.sendFile(path.join(__dirname, "notes.html"));
+// })
 
 // Declare API routes
 // GET method
 // app.get("/api/notes", function(request, response){
-//     response.json()
+//     fs.readFile(path.join(__dirname, "db.json"), function(err){
+//         if(err){
+//             throw err
+//         }
+//     });
 // })
 
 // POST method
