d34f47fc8692b73943d33fedbee366e16f1f9865
Steven Bong <stevenseal99@yahoo.com>
2020-10-08 11:24:27 -0700

    Had tutor session with ANdres. Worked on the server.js by changing the HTML and API routes. Add few method to the POST and DELETE API routes. Changed the file route in the HTML route

--- Develop/server.js
+++ Develop/server.js
@@ -5,62 +5,83 @@ const fs = require("fs");
 
 // Declare the express variables
 var app = express();
-var PORT = 8080;
-// var PORT = process.env.PORT || 8080;
+// var PORT = 8080;
+var PORT = process.env.PORT || 8080;
+
+// Declare the variables
+var notesArr = [];
 
 // Set the express for parsing the data
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
-// app.use(express.static("public"));
-
-// Declare the variables
-var notesArr = [{
-    "title": "Bla bla bla",
-    "text": "wassup",
-}];
+app.use(express.static("public"));
 
 // Declare the routes to display HTML page
 // Display the index.html file
-app.get("*", function(request, response){
-    response.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function(request, response){
+    response.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
 // Display the notes.html
-// app.get("/notes", function(request, response){
-//     response.sendFile(path.join(__dirname, "notes.html"));
-// })
+app.get("/notes", function(request, response){
+    response.sendFile(path.join(__dirname, "./public/notes.html"));
+})
 
 // Declare API routes
 // GET method
-// app.get("/api/notes", function(request, response){
-//     fs.readFile(path.join(__dirname, "db.json"), function(err){
-//         if(err){
-//             throw err
-//         }
-//     });
-// })
+app.get("/api/notes", function(request, response){
+    fs.readFile("./db/db.json", function(err, data){
+        if(err){
+            throw err
+        }
+        return response.json(JSON.parse(data))
+    });
+})
 
 // POST method
-// app.post("/api/notes", function(request, response){
-//     var newNote = request.body;
-//     console.log(newNote);
-
-//     notesArr.push(newNote);
-
-//     response.json(newNote);
-// })
+app.post("/api/notes", function(request, response){
+    request.body["id"] = notesArr.length + 1;
+    var newNote = JSON.stringify(request.body);
+    // console.log(request);
+    // console.log(request.body);
+    notesArr.push(newNote);
+    // console.log(notesArr);
+    fs.writeFile("./db/db.json", `[${notesArr}]`, 'utf-8', function(err){
+        if(err){
+            throw err
+        }
+        return response.json(request.body);
+    })
+})
 
 // DELETE method
-// app.delete("/api/notes/:id", function(request, response){
-//     var sorting = request.params.id;
-//     console.log(sorting);
+app.delete("/api/notes/:id", function(request, response){
+    var sorting = request.params.id;
+    // console.log(sorting);
     
-//     for(let i = 0; i < notesArr.length; i++){
-//         if(sorting === notesArr[i].id){
-//             response.json()
-//         }
-//     }
-// })
+    fs.readFile("./db/db.json", function(err, data){
+        if(err){
+            throw err
+        }
+
+        var notesHistory = JSON.parse(data);
+        // console.log(notesHistory)
+        for(let i = 0; i < notesArr.length; i++){
+            if(sorting == notesHistory[i].id){
+                notesArr.splice(i, 1)
+                // console.log(notesArr)
+                fs.writeFile("./db/db.json", `[${notesArr}]`, 'utf-8', function(err){
+                    if(err){
+                        throw err
+                    }
+                    return 
+                })
+            }
+        }
+    })
+    
+    response.end()
+})
 
 // Declare the listen function
 app.listen(PORT, function(){
